8aa04431bd09aad234a7fda54cff2cecfb7398f2
Tulir Asokan <tulir@maunium.net>
2023-11-03 01:20:19 +0200

    Add x86 offsets for 11.5 - 11.7

--- nac/offsets.go
+++ nac/offsets.go
@@ -5,15 +5,24 @@ import (
 	"fmt"
 )
 
+// Offsets from the macOS 11.7.7 binary for x86, works on 11.5 - 11.7
+var offsetsx86_11_7_7 = imdOffsets{
+	ReferenceSymbol:            "IDSProtoKeyTransparencyTrustedServiceReadFrom",
+	ReferenceAddress:           0xa3b8e,
+	NACInitAddress:             0x3d4870,
+	NACKeyEstablishmentAddress: 0x427390,
+	NACSignAddress:             0x3c71a0,
+}
+
 // offsets is a map from sha256 hash of identityservicesd to the function pointer offsets in that binary.
 var offsets = map[[32]byte]imdOffsets{
-	// macOS 11.5.1 (maybe any 11.5.x)
-	hexToByte32("e9ae1e7f0ef671269bc0b5f3e6791472665c7d17f8e3a3aead6276d15589cd4f"): {},
-	// macOS 11.6.1 (maybe any 11.6.x)
-	hexToByte32("f3467734b116f78c22cbe43217d7a337d3cf4dbbc58c0dde81f90dfa19d22e91"): {},
-	// macOS 11.7.7 (maybe any 11.7.x)
-	hexToByte32("80107d249088d9762ec38c8f86d6797b5070d476377e7c5ddacf83ad32d00a1e"): {},
-	// macOS 13.5 - 13.6 (possibly earlier versions too)
+	// macOS 11.5.1
+	hexToByte32("e9ae1e7f0ef671269bc0b5f3e6791472665c7d17f8e3a3aead6276d15589cd4f"): offsetsx86_11_7_7,
+	// macOS 11.6.1
+	hexToByte32("f3467734b116f78c22cbe43217d7a337d3cf4dbbc58c0dde81f90dfa19d22e91"): offsetsx86_11_7_7,
+	// macOS 11.7.7
+	hexToByte32("80107d249088d9762ec38c8f86d6797b5070d476377e7c5ddacf83ad32d00a1e"): offsetsx86_11_7_7,
+	// macOS 13.5 - 13.6 (possibly earlier versions too) - arm64
 	hexToByte32("fff8db27fef2a2b874f7bc6fb303a98e3e3b8aceb8dd4c5bfa2bad7b76ea438a"): {
 		ReferenceSymbol:            "IDSProtoKeyTransparencyTrustedServiceReadFrom",
 		ReferenceAddress:           0xb524c,
